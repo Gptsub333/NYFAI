ed4862b365ced9ae7e0629916af80cdeb1dc32f4
shivam-holboxai <shivam@holbox.ai>
2025-10-09 12:20:29 +0530

    add font change to articles page

--- app/media/Article/page.jsx
+++ app/media/Article/page.jsx
@@ -528,8 +528,8 @@ export default function Articles() {
       <div className="bg-gradient-to-r  text-white pt-20 relative overflow-hidden">
         <div className="absolute inset-0 bg-black/10"></div>
         <div className="max-w-7xl mx-auto px-4 md:px-8 relative z-10">
-          <div className="flex flex-col items-center justify-between gap-4">
-            <h1 className="text-5xl md:text-6xl font-bold mb-6 text-balance">Articles & Insights</h1>
+          <div className="flex flex-col items-center justify-between gap-2">
+            <h1 className="text-4xl md:text-4xl font-bold mb-6 text-balance">Articles & Insights</h1>
             <div>
 
               <p className="text-xl text-blue-100 max-w-4xl leading-relaxed text-center">
@@ -541,7 +541,7 @@ export default function Articles() {
             <button
               onClick={() => setIsAddDialogOpen(true)}
               disabled={apiLoading}
-              className="bg-[#1a729c] backdrop-blur-sm border border-white/20 text-white px-8 py-4 rounded-xl font-medium hover:bg-[#010817]/80 transition-all duration-300 flex items-center gap-3 shadow-lg disabled:opacity-50 disabled:cursor-not-allowed"
+              className="bg-[#1a729c] backdrop-blur-sm border border-white/20 text-white px-5 py-2 rounded-xl font-medium hover:bg-[#010817]/80 transition-all duration-300 flex items-center gap-3 shadow-lg disabled:opacity-50 disabled:cursor-not-allowed"
             >
               <Plus className="w-5 h-5" />
               Add Article
